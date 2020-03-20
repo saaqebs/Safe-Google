d2af0b2f9bec158af13b384b2d7c557d9b6a6daf
Saaqeb <ss3759@cornell.edu>
2020-03-20 04:41:10 -0400

    porting the website to jsx from html

---
+++ search.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+// import styles from './styles/styles.module.css';
+import './styles/styles.css';
+
+class Search extends Component {
+  render() {
+    return (
+      <html lang="en">
+        <head>
+          <link rel="stylesheet" href="./styles/style.css" />
+          <link rel="shortcut icon" type="image/x-icon" href="./images/google.jng" />
+          <meta charset="utf-8" />
+          <meta name="author" content="Saaqeb Siddiqi">
+
+          <title> Safe Google </title>
+          <script src="./script/script.js"> </script>
+        </head>
+        <body>
+          <div class="horizontal">
+            <img src="./images/google.png" alt="Google" />
+          </div>
+
+          <form action="https://www.google.com/search?">
+            <input name="hl" type="hidden" value="en">
+            <div class="horizontal">
+              <input name="q" type="text" autocomplete="off" maxlength="2048"
+                  autofocus id="searchbar" required minlength="2" />
+            </div>
+
+            <div class="horizontal">
+              <button type="submit" class="searchbutton"> Safe Search </button>
+            </div>
+          </form>
+
+        </body>
+      </html>
+    );
+  }
+}
+
+export default Search;
