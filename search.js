06c5d5eabfc33705668173eca3339e1d6a1ebf5d
Saaqeb <ss3759@cornell.edu>
2020-03-20 04:47:37 -0400

    ported css and images, problems with css in react app

--- search.js
+++ search.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 // import styles from './styles/styles.module.css';
-import './styles/styles.css';
+import './styles/style.css';
+import google from './images/google.png';
 
 class Search extends Component {
   render() {
@@ -11,18 +12,18 @@ class Search extends Component {
           <link rel="stylesheet" href="./styles/style.css" />
           <link rel="shortcut icon" type="image/x-icon" href="./images/google.jng" />
           <meta charset="utf-8" />
-          <meta name="author" content="Saaqeb Siddiqi">
+          <meta name="author" content="Saaqeb Siddiqi" />
 
           <title> Safe Google </title>
           <script src="./script/script.js"> </script>
         </head>
         <body>
           <div class="horizontal">
-            <img src="./images/google.png" alt="Google" />
+            <img src={google} alt="Google" />
           </div>
 
           <form action="https://www.google.com/search?">
-            <input name="hl" type="hidden" value="en">
+            <input name="hl" type="hidden" value="en" />
             <div class="horizontal">
               <input name="q" type="text" autocomplete="off" maxlength="2048"
                   autofocus id="searchbar" required minlength="2" />
