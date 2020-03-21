63c956f4507df5261e9b4ad2568d58c4f9583f26
Saaqeb <ss3759@cornell.edu>
2020-03-21 17:35:13 -0400

    modularizing search jsx

--- search.js
+++ search.js
@@ -1,42 +1,60 @@
 import React, { Component } from 'react';
 
-// import styles from './styles/styles.module.css';
 import style from './styles/style.module.css';
 import google from './images/google.png';
+
+
+function Head(props) {
+  return (
+    <head>
+      <link rel="stylesheet" href="./styles/style.css" />
+      <link rel="shortcut icon" type="image/x-icon" href="./images/google.jng" />
+      <meta charset="utf-8" />
+      <meta name="author" content="Saaqeb Siddiqi" />
+
+      <title> Safe Google </title>
+      <script src="./script/script.js"> </script>
+    </head>
+  );
+}
+
+function SearchForm(props) {
+  return (
+    <form action="https://www.google.com/search?">
+      <input name="hl" type="hidden" value="en" />
+      <div className={style.horizontal}>
+        <input name="q" type="text" autocomplete="off" maxlength="2048"
+            autofocus="autofocus" onfocus="this.select()"
+            id={style.searchbar} required minlength="2" />
+      </div>
+
+      <div className={style.horizontal}>
+        <button type="submit" className={style.searchbutton}>
+          Safe Search
+        </button>
+      </div>
+    </form>
+  );
+}
+
+function Body(props) {
+  return (
+    <body>
+      <div className={style.horizontal}>
+        <img src={google} alt="Google" />
+      </div>
+      <SearchForm />
+    </body>
+  )
+}
+
 
 class Search extends Component {
   render() {
     return (
       <html lang="en">
-        <head>
-          <link rel="stylesheet" href="./styles/style.css" />
-          <link rel="shortcut icon" type="image/x-icon" href="./images/google.jng" />
-          <meta charset="utf-8" />
-          <meta name="author" content="Saaqeb Siddiqi" />
-
-          <title> Safe Google </title>
-          <script src="./script/script.js"> </script>
-        </head>
-        <body>
-          <div className={style.horizontal}>
-            <img src={google} alt="Google" />
-          </div>
-
-          <form action="https://www.google.com/search?">
-            <input name="hl" type="hidden" value="en" />
-            <div className={style.horizontal}>
-              <input name="q" type="text" autocomplete="off" maxlength="2048"
-                  autofocus id={style.searchbar} required minlength="2" />
-            </div>
-
-            <div className={style.horizontal}>
-              <button type="submit" className={style.searchbutton}>
-                Safe Search
-              </button>
-            </div>
-          </form>
-
-        </body>
+        <Head />
+        <Body />
       </html>
     );
   }
